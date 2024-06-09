2100f64c96a48f26778673bb4877223d9e18b350
varevacdamir <37981465+varevacdamir@users.noreply.github.com>
2024-06-09 17:56:20 +0200

    App start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'home',
+		title: 'Slikaj Osvoji',
+		component: HomeComponent
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'home'
+	},
+	{
+		path: '**',
+		component: PageNotFoundComponent
+	}
+];
